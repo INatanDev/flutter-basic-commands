07472f76ddf90bc1f99b584668dc21bf8798716c
INatanDev <natan.ferreira21@hotmail.com>
2023-08-14 09:10:17 -0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/meu_projeto_estudos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.meu_projeto_estudos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    height: 100,
+                    width: 100,
+                  ),
+                  Container(
+                    color: Colors.orange,
+                    height: 100,
+                    width: 100,
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    height: 100,
+                    width: 100,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    color: Colors.green,
+                    height: 100,
+                    width: 100,
+                  ),
+                  Container(
+                    color: Colors.blue.shade300,
+                    height: 100,
+                    width: 100,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 100,
+                    width: 100,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    color: Colors.purple,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.pink.shade300,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
